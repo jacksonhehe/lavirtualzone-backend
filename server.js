ae37b690c008a4cfa01e7edb0d2129d054d93388
Jack Cano Rodriguez <JackCanoRodriguez@hotmail.com>
2025-02-22 19:47:34 -0500

    Update to mongoose 7.6.0 and clean build

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error de conexión:', err));
 
