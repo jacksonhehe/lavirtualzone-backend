7f007fe2d0e837d68d52a470c2f91366fabd4244
Jack Cano Rodriguez <JackCanoRodriguez@hotmail.com>
2025-04-01 14:54:08 -0500

    Actualizacio

---
+++ public/js/config.js
@@ -0,0 +1,6 @@
+// public/js/config.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("config.js cargado correctamente.");
+    // Aquí puedes añadir cualquier configuración adicional que necesites.
+  });
+  
